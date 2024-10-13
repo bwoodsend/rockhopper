f1d63c5b14342cfb54b3a90794afbbd303a22f30
Brénainn Woodsend <bwoodsend@gmail.com>
2024-10-13 19:27:40 +0100

    Test/declare Python 3.13 support

--- setup.py
+++ setup.py
@@ -25,6 +25,7 @@ setup(
         'Programming Language :: Python :: 3.10',
         'Programming Language :: Python :: 3.11',
         'Programming Language :: Python :: 3.12',
+        'Programming Language :: Python :: 3.13',
     ],
     description="Ragged (rows with different lengths) 2D NumPy arrays.",
     install_requires=copy_requirements(),
